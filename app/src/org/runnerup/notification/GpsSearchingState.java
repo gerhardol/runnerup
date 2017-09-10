75999116c03cd62ae4159724cc8f52e771106f0a
gerhardol <gerhard.nospam@gmail.com>
2017-09-11 00:09:27 +0200

    Android O notification channels

--- app/src/org/runnerup/notification/GpsSearchingState.java
+++ app/src/org/runnerup/notification/GpsSearchingState.java
@@ -23,7 +23,8 @@ public class GpsSearchingState implements NotificationState {
         this.context = context;
         this.gpsInformation = gpsInformation;
 
-        builder = new NotificationCompat.Builder(context);
+        String chanId = NotificationStateManager.getChannelId(context);
+        builder = new NotificationCompat.Builder(context, chanId);
         Intent i = new Intent(context, MainLayout.class);
         i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         i.putExtra(Constants.Intents.FROM_NOTIFICATION, true);
